bf23ad0457e1fd171af32f448691136e9c371c40
Falaleev Maxim <max@studio107.ru>
2016-06-22 17:27:35 +0300

    raw

---
+++ src/World.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Максим Фалалеев on 22/06/16.
+//
+
+#include "World.h"
---
+++ src/World.h
@@ -0,0 +1,17 @@
+//
+// Created by Максим Фалалеев on 22/06/16.
+//
+
+#ifndef FASTCRAFT_WORLD_H
+#define FASTCRAFT_WORLD_H
+
+namespace fastcraft {
+
+    class World {
+    public:
+        World();
+    };
+
+}
+
+#endif //FASTCRAFT_WORLD_H
